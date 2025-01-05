589eb9c415d299a79562a850edf29fc737acf794
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2025-01-05 16:45:01 +0330

    adding comments

--- bot/functions/init-user.php
+++ bot/functions/init-user.php
@@ -1,7 +1,9 @@
 <?php
+//===============  User Initialization:  =============
 
 //check if user exists in db
 $db_user = $db->q("SELECT * FROM tbl_users WHERE tg_id = ?", [$tg_id]);
+
 //if user exists, update username and tg_name
 if (isset($db_user[0])) {
     $db->q("UPDATE tbl_users SET username = ?, tg_name = ? WHERE tg_id = ?", [$username, $first_name . $last_name, $tg_id]);
