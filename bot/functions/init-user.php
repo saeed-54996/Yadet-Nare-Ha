7d21166c66f100079435a0dbca52de5d2c2561f3
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2025-01-05 16:29:39 +0330

    pt

--- bot/functions/init-user.php
+++ bot/functions/init-user.php
@@ -4,5 +4,5 @@
 
 //$db->q("INSERT INTO tbl_users (username, tg_name, tg_id) VALUES (?, ?, ?)", [$username,$first_name.$last_name, $user_id]);
 
-$res = $db->q("SELECT * FROM tbl_users");
+$res = $db->q("SELECT * FROM tbl_users WHERE id = 55");
 bot("sendMessage", ['chat_id' => ADMIN_ID, 'text' => json_encode($res)]);
