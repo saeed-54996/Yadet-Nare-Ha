61dd2f3f06349115dc271e5c4c1e534529c5ae47
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2025-01-05 16:28:25 +0330

    pt

--- bot/functions/init-user.php
+++ bot/functions/init-user.php
@@ -2,4 +2,7 @@
 
 //$db->q("SELECT * FROM users WHERE username = ? AND age > ?", [$username, $age]);
 
-$db->q("INSERT INTO tbl_users (username, tg_name, tg_id) VALUES (?, ?, ?)", [$username,$first_name.$last_name, $user_id]);
+//$db->q("INSERT INTO tbl_users (username, tg_name, tg_id) VALUES (?, ?, ?)", [$username,$first_name.$last_name, $user_id]);
+
+$res = $db->q("SELECT * FROM tbl_users");
+bot("sendMessage", ['chat_id' => ADMIN_ID, 'text' => json_encode($res)]);
