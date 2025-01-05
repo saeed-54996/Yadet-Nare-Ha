20606e60efbbaa2aee0d16e4c4dbe9c782740467
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2025-01-05 23:07:32 +0330

    feat: add user step tracking functionality

--- bot/functions/init-user.php
+++ bot/functions/init-user.php
@@ -7,6 +7,15 @@ $db_user = $db->q("SELECT * FROM tbl_users WHERE tg_id = ?", [$tg_id]);
 //if user exists, update username and tg_name
 if (isset($db_user[0])) {
     $db->q("UPDATE tbl_users SET username = ?, tg_name = ? WHERE tg_id = ?", [$username, $first_name . $last_name, $tg_id]);
+    $user_step = $db_user[0]['step'];
 } else { //if user does not exist, insert user into db
     $db->q("INSERT INTO tbl_users (username, tg_name, tg_id) VALUES (?, ?, ?)", [$username, $first_name . $last_name, $tg_id]);
+}
+
+
+//function to update user step in db:
+function update_step($step)
+{
+    global $db, $tg_id;
+    $db->q("UPDATE tbl_users SET step = ? WHERE tg_id = ?", [$step, $tg_id]);
 }
