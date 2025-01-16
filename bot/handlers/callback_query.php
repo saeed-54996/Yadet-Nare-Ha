1db2c5b8133853f7e7468fea509a0c55774a7360
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2025-01-16 23:39:52 +0330

    fix: add task count display and navigation for task lists in callback query handling

--- bot/handlers/callback_query.php
+++ bot/handlers/callback_query.php
@@ -36,8 +36,38 @@ if (preg_match('/^([a-z_]+)_(\d+)$/', $cdata, $matches)) {
     $order = $matches[1];
     $list_id = $matches[2];
 
-    if($order == "view_tasks"){
+    if ($order == "view_tasks") {
         
+        $list_id = $order;
+        $list_tasks = $db->q("SELECT * FROM tbl_tasks WHERE list_id = ?", [$list_id]);
+        
+        if($list_tasks[0]){
+        $list_tasks_count = count($list_tasks);
+        
+        $text = "تعداد $list_tasks_count وظیفه ناتمام برای این لیست وجود دارد.";
+        bot("editMessageText", [
+            'chat_id' => $chat_id,
+            'text' => $text,
+            'reply_markup' => [
+                'inline_keyboard' => [
+                    [['text' => 'مشاهده وظایف 10 روز آینده 📋', 'callback_data' => 'view_10_tasks_' . $list_id]],
+                    [['text' => 'مشاهده وظایف 30 روز آینده 🗓', 'callback_data' => 'view_30_tasks_' . $list_id]],
+                ]
+            ]
+        ]);
+        }
+        else{
+            $text = "هیچ وظیفه ای برای این لیست وجود ندارد";
+            bot("editMessageText", [
+                'chat_id' => $chat_id,
+                'text' => $text,
+                'reply_markup' => [
+                    'inline_keyboard' => [
+                        [['text' => 'بازگشت به لیست ها', 'callback_data' => 'view_list_'.$list_id]],
+                    ]
+                ]
+            ]);
+        }
     }
 
 
