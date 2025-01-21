4d00cd21dca65773dea49280f4ea2a74fb5d3b0c
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2025-01-21 04:06:23 +0330

    fix: add missing semicolon in user initialization logic

--- bot/functions/init-user.php
+++ bot/functions/init-user.php
@@ -8,6 +8,7 @@ $db_user = $db->q("SELECT * FROM tbl_users WHERE tg_id = ?", [$tg_id]);
 if (isset($db_user[0])) {
     $db->q("UPDATE tbl_users SET username = ?, tg_name = ? WHERE tg_id = ?", [$username, $first_name . $last_name, $tg_id]);
     $user_step = $db_user[0]['step'];
+    $user_db_id = $db_user[0]['id']
 } else { //if user does not exist, insert user into db
     $db->q("INSERT INTO tbl_users (username, tg_name, tg_id) VALUES (?, ?, ?)", [$username, $first_name . $last_name, $tg_id]);
 }
