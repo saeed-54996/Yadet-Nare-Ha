ee8d44d2bef31f67d48e68875ce4de2e6f0c5372
Saeed Ghorbani <ghorbanisaeed82@gmail.com>
2025-01-23 00:57:44 +0330

    feat: implement task notification system for upcoming tasks based on subscription

--- bot/services/reminder.php
+++ bot/services/reminder.php
@@ -2,32 +2,41 @@
 
 require_once __DIR__ . '/../functions/jalaliToUnix.php';
 require_once __DIR__ . '/../../db.php';
+require_once __DIR__ . '/../core.php';
 
 $db = new Database();
 
-//date_default_timezone_set('Asia/Tehran');
-
-
 $current_time = new DateTime();
-$sub = $db->q('SELECT * FROM `tbl_tasks` WHERE task_date IS NOT NULL AND is_deleted = 0');
-foreach ($sub as $s){
-  $checktime = new DateTime($s['task_date']);
-  $between = $current_time->diff($checktime);
-  // %R = + or -
-  // %a = days
-  // %H = hours
-  // %i = minutes
-  // %s = seconds
-  if($between->format('%R%a') == "+1"){
-    // task is tomorrow;
-  }else if ($between->format('%R%a') == "+0"){
-    // task is today;
-  }else if ($between->format('%R%a') == "-1"){
-    // task was yesterday;
+$tasks = $db->q('SELECT t.*, l.is_deleted as list_deleted 
+    FROM `tbl_tasks` t
+    JOIN `tbl_notification_lists` l ON t.list_id = l.id
+    WHERE t.task_date IS NOT NULL AND t.is_deleted = 0 AND t.is_end = 0
+');
+
+foreach ($tasks as $task) {
+    // Check if the task's list is deleted
+    if ($task['list_deleted'] == 1) {
+        continue;
     }
 
-  //echo $s['task_name'] . " == " . $between->format('%R%a days, %H hours');
-  //echo "<br>";
-};
-      
+    $checktime = new DateTime($task['task_date']);
+    $between = $current_time->diff($checktime);
 
+    $days = (int)$between->format('%R%a');
+
+    if ($days == 1 || $days == 3 || $days == 7) {
+        // Send notification to users in the subscription list
+        $subscribers = $db->q('SELECT * FROM `tbl_list_subscribers` WHERE list_id = ?', [$task['list_id']]);
+        foreach ($subscribers as $subscriber) {
+            // Send notification to $subscriber['user_id']
+            // You can use your preferred method to send notifications (e.g., email, SMS, etc.)
+            sendNotification($subscriber['user_id'], $task['task_name'], $days);
+        }
+    }
+}
+
+function sendNotification($userId, $taskName, $days) {
+    // Implement your notification logic here
+    // For example, you can send an email or SMS to the user
+    echo "Notification sent to user $userId for task '$taskName' which is due in $days days.\n";
+}
